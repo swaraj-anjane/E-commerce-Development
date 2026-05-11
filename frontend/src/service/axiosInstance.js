8a03f1a96b7a3f354c2809d93a15a8af66359325
Swaraj <swarajanjane0@gmail.com>
2026-05-11 18:32:23 +0530

    connected render backend

--- frontend/src/service/axiosInstance.js
+++ frontend/src/service/axiosInstance.js
@@ -1,10 +1,11 @@
 import axios from "axios";
 
 const axiosInstance = axios.create({
-  baseURL: import.meta.env.VITE_PUBLIC_API_URL,
+  baseURL: "https://e-commerce-development.onrender.com",
+  
+  // baseURL: import.meta.env.VITE_PUBLIC_API_URL,
+  
   withCredentials: true,
 });
-
-
 
 export default axiosInstance;
